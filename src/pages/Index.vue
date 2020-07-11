748301bb62a5ebebf221d905f9986f16ca900bc7
Victor H Valle <victor.valle@lkmx.io>
2020-07-10 19:20:26 -0700

    Committing index so it's used in the other computer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,37 @@
 <template>
   <default-layout>
+
     <h1>Stake Your Tokens with Chain of Secrets</h1>
     <p>Join us to earn rewards on your staked SCRT and help secure the Enigma Blockchain while reducing your risk of slashing with our enterprise infrastrcture.</p>
     <a href="">Delegate With Us</a>
+    
+    <h2>Why Chain of Secrets?</h2>
+    <p>These are the core Chain of Secrets' values that we bring to each project we embark on within the Enigma community.</p>
+
+    <h3>Reliability</h3>
+    <p>Our infrastructure is housed in highly-secure datacenters around the world. We are constantly building redundancy into our infrastructure to ensure perpetual node coverage and network uptime.</p>
+
+    <h3>Engagement</h3>
+    <p>Our mission as a privacy validator is to build engagement on the Engima Blockchain with exciting new tools, applications &amp; services.</p>
+
+    <h3>Trust</h3>
+    <p>We believe in building decentralization and are committed to provide our projects to the Enigma community in an open-source manner.</p>
+
+    <h2>About</h2>
+    <p>Chain of Secrets is a Privacy Validator for the Enigma network. We offer delegation services to individuals and business investors looking to maximize their ROI and see the benefit of a privacy layer for the Internet of Blockchains. We actively participate in the development and governance of the Enigma Blockchain ecosystem.</p>
+    <p>We are a team of technologists with backgrounds in security, networking, blockchain infrastructure, token economics, and decentralized finance.</p>
+
+    <h2>Chain of Secrets Roadmap</h2>
+    <p>Below is our 2020-2021 roadmap, focused exclusively on supporting the Enigma Protocol. We'll keep it updated so check back to see how it evolves as we progress toward the vision of decentralized privacy.</p>
+
+    <h2>Our Validator Infrastructure</h2>
+
+
+    <h2>Chain of Secrets Team</h2>
+    <p>Founded by blockchain technologists who are experienced in enterprise infrastructure and applications.</p>
+    <p>We bring our passion, commitment and breadth of expertise to bear on any size project.</p>
+
+    
   </default-layout>
 </template>
 
